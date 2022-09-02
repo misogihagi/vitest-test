c4b4a2ac29e75d234de270a8b82c6834ea09faba
misogihagi <47350059+misogihagi@users.noreply.github.com>
2022-09-03 07:17:57 +0900

    init vitest

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,22 @@ export default defineConfig({
   build: {
     target: 'esnext',
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    transformMode: {
+        web: [/\.[jt]sx?$/],
+    },
+    setupFiles: './setupVitest',
+    // solid needs to be inline to work around
+    // a resolution issue in vitest
+    // And solid-testing-library needs to be here so that the 'hydrate'
+    // method will be provided
+    deps: {
+        inline: [/solid-js/, /solid-testing-library/],
+    },
+},
+resolve: {
+    conditions: ['development', 'browser'],
+},
 });
